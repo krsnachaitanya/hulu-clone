632f3dcabedbdf6bb3dd8c7615be2e2f9dec4cdc
Chaitu <chaitu.ca539@gmail.com>
2021-06-01 13:13:36 +0530

    header item done'

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import Head from "next/head";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+
 export default function Home() {
   return (
-    <div>
+    <>
+      <Head>
+        <title>Hulu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Nav />
       <h1>Hulu Clone</h1>
-    </div>
+    </>
   );
 }
