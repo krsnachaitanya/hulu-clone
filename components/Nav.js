007c7608fcd0e0ee15812a103974dc30e0d5f697
Chaitu <chaitu.ca539@gmail.com>
2021-06-02 18:35:25 +0530

    add role button to navigation

--- components/Nav.js
+++ components/Nav.js
@@ -6,12 +6,13 @@ const Nav = () => {
 
   return (
     <nav className="relative">
-      <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
+      <div className="flex px-10 space-x-10 overflow-x-scroll text-2xl sm:px-20 whitespace-nowrap sm:space-x-20 scrollbar-hide">
         {Object.entries(requests).map(([key, { title }]) => (
           <h2
-            className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500 select-none"
+            className="transition duration-100 transform cursor-pointer select-none last:pr-24 hover:scale-125 hover:text-white active:text-red-500"
             key={key}
             onClick={() => router.push(`/?genre=${key}`)}
+            role="button"
           >
             {title}
           </h2>
