7a2c4471d6f731db912bbc35cacdd079751ea184
Chaitu <chaitu.ca539@gmail.com>
2021-06-02 18:44:16 +0530

    remove tap highlight

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,6 +1,9 @@
 const HeaderItem = ({ title, Icon }) => {
   return (
-    <div className="group flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white">
+    <div
+      role="button"
+      className="no-tap-highlight group flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white"
+    >
       <Icon className="h-8 mb-1 group-hover:animate-bounce " />
       <p className="tracking-widest opacity-0 group-hover:opacity-100">
         {title}
--- components/Nav.js
+++ components/Nav.js
@@ -9,7 +9,7 @@ const Nav = () => {
       <div className="flex px-10 space-x-10 overflow-x-scroll text-2xl sm:px-20 whitespace-nowrap sm:space-x-20 scrollbar-hide">
         {Object.entries(requests).map(([key, { title }]) => (
           <h2
-            className="transition duration-100 transform cursor-pointer select-none last:pr-24 hover:scale-125 hover:text-white active:text-red-500"
+            className="transition duration-100 transform cursor-pointer select-none  no-tap-highlight last:pr-24 hover:scale-125 hover:text-white active:text-red-500"
             key={key}
             onClick={() => router.push(`/?genre=${key}`)}
             role="button"
